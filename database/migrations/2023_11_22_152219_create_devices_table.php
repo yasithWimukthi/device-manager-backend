6c0505f825e8c39c15b9258469da5679d992de87
yasithw <yasith.w@eight25media.com>
2023-11-22 21:32:09 +0530

    implement device management

---
+++ database/migrations/2023_11_22_152219_create_devices_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->id();
+            $table->integer('number')->unique();
+            $table->string('type');
+            $table->string('image');
+            $table->date('date_created');
+            $table->enum('status', ['active', 'inactive']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('devices');
+    }
+};
