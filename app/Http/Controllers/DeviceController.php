7b81f7122eadedcf82f64b6df3061ad8285db08b
yasithw <yasith.w@eight25media.com>
2023-11-22 23:37:22 +0530

    implement device management

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -8,30 +8,49 @@ use Illuminate\Http\Request;
 class DeviceController extends Controller
 {
     /**
+     * get all devices
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        try {
+            $devices = Device::all();
+            return response()->json(['message' => 'Devices retrieved successfully', 'data' => $devices], 200);
+        }catch (\Exception $e){
+            return response()->json(['message' => 'Devices retrieval failed', 'data' => $e->getMessage()], 500);
+        }
+    }
+
+    /**
      * store device details
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
-            'number' => 'required|integer|unique:devices',
-            'type' => 'required|string',
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048', // Allow only image files
-            'date_created' => 'required|date',
-            'status' => 'required|in:active,inactive',
-        ]);
+        try {
+            $validatedData = $request->validate([
+                'number' => 'required|integer|unique:devices',
+                'type' => 'required|string',
+                'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048', // Allow only image files
+                'date_created' => 'required|date',
+                'status' => 'required|in:active,inactive',
+            ]);
 
-        $imagePath = $validatedData['image']->store('images', 'public');
+            $imagePath = $validatedData['image']->store('images', 'public');
 
-        $device = Device::create([
-            'number' => $validatedData['number'],
-            'type' => $validatedData['type'],
-            'image' => $imagePath,
-            'date_created' => $validatedData['date_created'],
-            'status' => $validatedData['status'],
-        ]);
+            $device = Device::create([
+                'number' => $validatedData['number'],
+                'type' => $validatedData['type'],
+                'image' => $imagePath,
+                'date_created' => $validatedData['date_created'],
+                'status' => $validatedData['status'],
+            ]);
 
-        return response()->json(['message' => 'Device created successfully', 'data' => $device], 201);
+            return response()->json(['message' => 'Device created successfully', 'data' => $device], 201);
+        }catch (\Exception $e){
+            return response()->json(['message' => 'Device creation failed', 'data' => $e->getMessage()], 500);
+        }
+
     }
 }
